e6d03eb7241920f756f11e84f5b571b169d5bf18
Chi Trinh <Chikun_88@hotmail.com>
2019-05-07 10:55:05 +0200

    Implements Serializable & changed class name to Tracks

--- src/main/java/com/project/playlist/domain/Tracks.java
+++ src/main/java/com/project/playlist/domain/Tracks.java
@@ -1,6 +1,8 @@
 package com.project.playlist.domain;
 
-public class Song {
+import java.io.Serializable;
+
+public class Tracks implements Serializable{
 
 	private String artist;
 	private String title;
@@ -8,7 +10,7 @@ public class Song {
 	private String album;
 	private String length;
 		
-	public Song(String artist, String title, String genre, String album, String length) {
+	public Tracks(String artist, String title, String genre, String album, String length) {
 		this.artist = artist;
 		this.title = title;
 		this.genre = genre;
