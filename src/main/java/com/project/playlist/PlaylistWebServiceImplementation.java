e2fc9c7f3d985a6cf454797eb4894aea84ef3c4b
Satomi <uk_blue_swed_yellow@hotmail.com>
2019-05-09 15:36:41 +0200

    Added PlaylistWebServiceImplementation in playlist package.

---
+++ src/main/java/com/project/playlist/PlaylistWebServiceImplementation.java
@@ -0,0 +1,30 @@
+package com.project.playlist;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.jws.WebService;
+
+import com.project.playlist.domain.Track;
+
+@Stateless
+@WebService(name="PlaylistWebService")
+public class PlaylistWebServiceImplementation {
+
+  @Inject
+  private PlaylistServiceLocal service;
+
+  public void registerNewTrack(Track track) {
+    service.registerTrack(track);
+  }
+
+  public List<Track> getAllTracks() {
+    return service.getAllTracks();
+  }
+
+  // public List<Track> searchByTitle(Track title) {
+  //   return service.searchByTitle(title);
+  // }
+
+}
