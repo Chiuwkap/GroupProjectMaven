a860d974fa4d9b3c54d8dc7c7d08db5910d29531
Satomi <uk_blue_swed_yellow@hotmail.com>
2019-05-16 12:44:27 +0200

    Added anotations etc in TrackResource in rest package.

--- src/main/java/com/project/playlist/rest/TrackResource.java
+++ src/main/java/com/project/playlist/rest/TrackResource.java
@@ -16,14 +16,75 @@ public class TrackResource {
   @Inject
   private PlaylistServiceLocal service;
 
+  @POST
+  @Path("/add/{}")
+  public void registerTrack(Track track) {
+
+    service.registerTrack(track);
+  }
+
+  @DELETE
+  @Path("/delete/{id}")
+  public void deleteTrack(@PathParam("id") int id) {
+
+    service.deleteTrack(id);
+  }
+
+  @POST
+  @Path("/update/{id}")
+  public void changeTrack(@PathParam("id") int id, String[] trackValues) {
+
+    service.changeTrack(id, trackValues);
+  }
+
+  @POST
+  @Path("/update/{id}")
+  public void changeTitle(@PathParam("id") int id, String title) {
+
+    service.changeTitle(id, title);
+  }
+
+  @POST
+  @Path("/update/{id}")
+  public void changeArtist(@PathParam("id") int id, String artist) {
+
+    service.changeArtist(id, artist);
+  }
+
+  @POST
+  @Path("/update/{id}")
+  public void changeAlbum(@PathParam("id") int id, String album) {
+
+    service.changeAlbum(id, album);
+  }
+
   @GET
+  @Path("/all-track")
   @Produces({"application/JSON"})
   public List<Track> getAllTracks() {
+
     return service.getAllTracks();
   }
 
-	// public List<Track> searchByTitle() {
-  //   return null;
-  // }
+  @GET
+  @Path("/search")
+  public List<Track> searchByTitle(@QueryParam("title") String title) {
+
+    return service.searchByTitle(title);
+  }
+
+  @GET
+  @Path("/search")
+  public List<Track> searchByArtist(@QueryParam("artist") String artist) {
+
+    return service.searchByArtist(artist);
+  }
+
+  @GET
+  @Path("/search/{album}")
+  public List<Track> searchByAlbum(@QueryParam("album") String album) {
+
+    return service.searchByAlbum(album);
+  }
 
 }
---
+++ src/main/java/com/project/playlist/rest/WebResourceTest.java
@@ -0,0 +1,4 @@
+package com.project.playlist.rest;
+
+public class WebResourceTest {
+}
