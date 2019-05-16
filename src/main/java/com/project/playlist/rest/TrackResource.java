d7590afd28e33ab850f54f3623b36d201d108207
Olof Kastrup <olofkastrup@gmail.com>
2019-05-16 15:59:26 +0200

    Unified search methods to one method

--- src/main/java/com/project/playlist/rest/TrackResource.java
+++ src/main/java/com/project/playlist/rest/TrackResource.java
@@ -4,7 +4,15 @@ import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import javax.ws.rs.*;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
 
 import com.project.playlist.PlaylistServiceLocal;
 import com.project.playlist.domain.Track;
@@ -66,25 +74,44 @@ public class TrackResource {
     return service.getAllTracks();
   }
 
+//  @GET
+//  @Path("/searchtitle")
+//  @Produces({"application/JSON"})
+//  public List<Track> searchByTitle(@QueryParam("title") String title) {
+//
+//    return service.searchByTitle(title);
+//  }
+//
+//  @GET
+//  @Path("/searchartist")
+//  @Produces({"application/JSON"})
+//  public List<Track> searchByArtist(@QueryParam("artist") String artist) {
+//
+//    return service.searchByArtist(artist);
+//  }
+//
+//  @GET
+//  @Path("/searchalbum")
+//  @Produces({"application/JSON"})
+//  public List<Track> searchByAlbum(@QueryParam("album") String album) {
+//
+//    return service.searchByAlbum(album);
+//  }
+  
   @GET
   @Path("/search")
-  public List<Track> searchByTitle(@QueryParam("title") String title) {
-
-    return service.searchByTitle(title);
+  @Produces({"application/JSON"})
+  public List<Track> search(@Context UriInfo info) {
+	  String title = info.getQueryParameters().getFirst("title");
+	  String artist = info.getQueryParameters().getFirst("artist");
+	  String album = info.getQueryParameters().getFirst("album");
+	  if (title != null) {
+		  return service.searchByTitle(title);
+	  } else if (artist != null) {
+		  return service.searchByArtist(artist);
+	  } else if (album != null) {
+		  return service.searchByAlbum(album);
+	  }
+	return null;
   }
-
-  @GET
-  @Path("/search")
-  public List<Track> searchByArtist(@QueryParam("artist") String artist) {
-
-    return service.searchByArtist(artist);
-  }
-
-  @GET
-  @Path("/search/{album}")
-  public List<Track> searchByAlbum(@QueryParam("album") String album) {
-
-    return service.searchByAlbum(album);
-  }
-
 }
